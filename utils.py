f0f79ba74b6405390527d46240e6d32691dfba92
Ashish Raj <21c47@bcebakhtiyarpur.org>
2024-03-16 04:58:06 +0530

    added custom function to convert a jpg image into grayscale and then using the grayscale image as test imput to the neural model .

--- utils.py
+++ utils.py
@@ -1,6 +1,21 @@
 import numpy as np
+from PIL import Image
 def load_data():
     X = np.load("data/X.npy")  # input data 
     y = np.load("data/y.npy")  # target values
     
     return X, y
+
+def convert_to_grayscale():
+    image = Image.open('assects/0.jpg')
+
+    grayscale_image = image.convert('L')
+    resized_image = grayscale_image.resize((20 , 20))
+    matrix = np.array(resized_image)
+
+    return matrix
+
+
+    
+ 
+
